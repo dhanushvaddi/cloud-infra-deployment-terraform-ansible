c3aff23bee874ae7e5be325e601c354a480ce98d
dhanushvaddi <dhanushh.vaddi@gmail.com>
2025-04-20 00:10:54 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "Terraform-Web"
+  }
+}
